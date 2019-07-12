5054983b7b997d9f16b7e57a6e48276575cbb213
CaetanoGS <50907344+CaetanoGS@users.noreply.github.com>
2019-07-13 01:19:22 +0200

    Update SS.ino

--- Controladores/SS/SS.ino
+++ Controladores/SS/SS.ino
@@ -1,182 +1,130 @@
-class mSS{
-public:
-  
-  double error, sample, lastSample, K, kI, I, x, U, setPoint, aux;
-  long lastProcess;
-  
-  // Método construtor
+#include <BasicLinearAlgebra.h>
 
-  mSS(double _K, double _kI){
-    K = _K;
-    kI = _kI;    
-  }
+using namespace BLA;
 
-  // Adicionar uma nova amostra
-  
-  void addNewSample(double _sample){
-    sample = _sample;
-  }
-  
-  // Adicionar Setpoint
+// Criando as Matrizes necessárias para o cálculo
 
-  void setSetPoint(double _setPoint){
-    setPoint = _setPoint;
-  }
-  
-  //Calculo do controle que é dado por u(t) = Ki*integral(Erro) + K*x(t)
-
-  double process(){
-
-    error = setPoint - sample; // Coloca-se sinal invertido por conta da característica do sistema
-
-    // Diferença de tempo entre as amostras
-
-          
-    float dT = (millis() - lastProcess) / 1000.0;
-    lastProcess = millis();
-    
-    // Ação integral
-
-    I += (error * dT);            
-
-    if(error < 0.2 && error > -0.2){
-      if((255 - aux)>255)
-        I = 0;
-      else if((255 - aux)<0)
-      I = I/1.2;
-  }else
-      I=I;
-
-    // Ação Derivativa para encontrar os estados x(t)
-
-     x = (lastSample-sample)/dT;
-     lastSample = sample;    
-    
-    // Ação de Controle
-
-    U = 60 -kI*I - K*x;
-    aux = U;
-
-    // Controle do intervalo de atuação  de acordo com as limitações do PWM
-
-    if (U > 255)
-      U = 255;
-    else if (U < 0)
-      U = 0;
-    else
-      U = U;
-      
-    
-    return U;
-  
-  }
-};
+BLA::Matrix<1,1> kE = {4.6249};
+BLA::Matrix<1,1> kI = {-5.0505};
+BLA::Matrix<1,1> Int;
+BLA::Matrix<1,1> x;
+BLA::Matrix<1,1> u;
+BLA::Matrix<1,1> ajuste = {60};
 
 
+#define N 100  
 
-// Mapeamento de constantes
-
-#define N 100
-#define K   5.2527
-#define Ki 3.3512
-#define K1 4.6249
-#define Ki1 -5.0505
-
-const int LM35 = A0;
-int cont = 0;
-double temperatura, input, output,output1, sum, setPoint = 30;
+const int LM35 = A0;   
+double error, setPoint, aux, Ts, temperatura,sum, output1;
+long dT, lastProcess = 0;
 float val[N];
 
-mSS MySS(K, Ki);
-mSS MySS1(K1, Ki1);
 
-void setup() {
+void setup(){
+
+  Serial.begin(9600);
+  setPoint = 30;
+  Ts = 25;
   
-  Serial.begin(9600); 
-  pinMode(LM35,INPUT);
-  MySS.setSetPoint(setPoint);
-  MySS1.setSetPoint(setPoint);
-
 }
 
-void loop() {
+void loop (){
 
-temperatura = (double(analogRead(LM35))*5/(1023))/0.01; //Leitura da temperatura baseando-se nos calculos do datasheet
+  // Definindo Ts = 25 ms
 
+  unsigned long lastTime;
+  unsigned long now = millis();
+  unsigned long timeChange = (now - lastTime);
 
-// Filtro janela móvel
+  if (timeChange>=Ts){
 
+    // Efetua a leitura do sensor
 
-for(int i = N-1; i>0; i--){
-  val[i] = val[i-1];
-}
+    temperatura = (double(analogRead(LM35))*5/(1023))/0.01;
 
-val[0] = temperatura;
+    // Filtra os dados coletados do sensor                
 
-sum = 0;
+    for(int i = N-1; i>0; i--){
+      val[i] = val[i-1];
+    }
 
-for(int i = 0; i < N; i++){
-  sum += val[i];
-}
+    val[0] = temperatura;
 
-sum = (sum/N);
+    sum = 0;
 
-temperatura = sum;
+    for(int i = 0; i < N; i++){
+      sum += val[i];
+    }
 
-// Inicio do controle
+    sum = (sum/N);
 
-MySS.addNewSample(temperatura);
-MySS1.addNewSample(temperatura);
+    temperatura = sum;
 
-if (cont >= 99){
+    error = getError(setPoint, temperatura);
+    dT = getDT(lastProcess);
+
+    Int += error*dT;
+
+    x = {temperatura};
+
+    u = ajuste - kE*x - kI*Int;
+
+    // Windup
+
+    if(error < 0.2 && error > -0.2){
+      if((255 - u(0))>255)
+        Int = Int/1.3;
+      else if((255-u(0))<0)
+        Int = Int/1.3;
+      }else
+        Int = Int;
+
+    if (u(0) > 255)
+      u = {255};
+    else if (u(0) < 0)
+      u = {0};
+    else 
+      u = u;
+
+    if((millis() >= (350000*0.6)) && (millis() <= (350000*0.67)))
+      output1 = 128;
+    else if(millis() >= (350000*1.6) && (millis() <= (350000*1.65)))
+      output1 = 160;
+    else if (millis() >= (350000*4.4) && (millis() <= (350000*4.8)))
+      output1 = 100;
+    else
+      output1 = 255;
+
+    // Escrevendo cálculos para controlar os atuadores
   
-  output = MySS.process();
-  output1 = MySS1.process();
-  //output1 = 255;
+    analogWrite(6,u(0));
+    analogWrite(5,output1);
 
-// Controle do atuador
-
-  analogWrite(6,output);
-  analogWrite(5,output1);
-
-  if (millis() > 350000 && millis() < (350000*2)){
-    setPoint = 31;
-    MySS.setSetPoint(setPoint);
-    MySS1.setSetPoint(setPoint);
+    // Imprimindo Resultados no Matlab
   
-  }else if(millis() >= (350000*2) && millis() < (350000*3)){
-    setPoint = 29.5;
-    MySS.setSetPoint(setPoint);
-    MySS1.setSetPoint(setPoint);
+    double results[] = {temperatura, u(0), output1, setPoint};
+    Serial.print(results[0]);
+    Serial.print("\t");
+    Serial.print(results[1]);  
+    Serial.print("\t");
+    Serial.print(results[2]);  
+    Serial.print("\t");
+    Serial.println(results[3]);
   
-  }else if(millis() >= (350000*3) && millis() < (350000*4)){
-    setPoint = 31.5;
-    MySS.setSetPoint(setPoint);
-    MySS1.setSetPoint(setPoint);
-    
-  }else if(millis() >= (350000*4)){
-    setPoint = 30.5;
-    MySS.setSetPoint(setPoint);
-    MySS1.setSetPoint(setPoint);
   }
-
-
-// Construção de vetores para exportação para o Matlab via serial
 }
-double results[] = {temperatura, output, output1, setPoint};
 
-Serial.print(results[0]);
-Serial.print("\t");
-Serial.print(results[1]); 
-Serial.print("\t");
-Serial.print(results[2]); 
-Serial.print("\t"); 
-Serial.println(results[3]);
+double getError(double setPoint, double temperatura){
 
-delay(1);
-if(cont <= 99)
-  cont++;
-else
-  cont = 100;
+  error = -(setPoint - temperatura);
 
+  return error;
+}
+
+long getDT(long lastProcess){
+
+  dT = (millis()-lastProcess)/1000;
+  lastProcess = millis();
+  
+  return dT;
 }
