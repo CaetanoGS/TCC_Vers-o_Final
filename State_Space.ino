f21e078686ebf41b34e17f6d1d19eeb0210b5953
CaetanoGS <50907344+CaetanoGS@users.noreply.github.com>
2019-07-23 04:12:36 +0200

    Update State_Space.ino

--- State_Space.ino
+++ State_Space.ino
@@ -20,14 +20,22 @@ BLA::Matrix<1,1> x;                                                       // Mat
 BLA::Matrix<1,1> u = {0};                                                       // Sinal de Controle
 BLA::Matrix<1,1> ajuste = {382.58};                                           // Sinal de controle inicia a partir de 60 para quebrar a inércia do cooler, alterar de acordo com o projeto
 
+/*
+BLA::Matrix<1,1> kE_L = {6.2163};                                           // Matriz de ganho estático
+BLA::Matrix<1,1> kI_L = {5.1600};                                          // Matriz de ganho dos integradores
+BLA::Matrix<1,1> Int_L;                                                     // Matriz contendo a integral do(s) erro(s)
+BLA::Matrix<1,1> u_L = {0};                                                       // Sinal de Controle
+BLA::Matrix<1,1> ajuste_L = {150}; 
+*/
 
 #define N 100                                                             // Janela do filtro        
 
 const int LM35 = A0;                                                      // Port de leitura do sensor de temperatura
-double error, setPoint, aux, Ts, temperatura,sum, output1;                
+double error, setPoint, aux, Ts, temperatura,sum;                
 float val[N];
 long cont = 0;
 long lastProcess;
+unsigned long lastTime = 0;
 
 
 void setup(){
@@ -42,7 +50,6 @@ void loop (){
 
   // Definindo Ts = 25 ms
 
-  unsigned long lastTime;
   unsigned long now = millis();
   unsigned long timeChange = (now - lastTime);
 
@@ -71,22 +78,16 @@ void loop (){
     temperatura = sum;
 
     
-    if(cont > N)
-      error = getError(setPoint, temperatura);
-
-    float dT = (millis() - lastProcess) / 1000.0;             // Definindo o intervalo de tempo entre as amostras para o cálculo da integral e da derivada
-    lastProcess = millis();
-
-    if(cont > N)
+    if(cont > N){
+      error = (setPoint - temperatura);
+      float dT = (millis() - lastProcess) / 1000.0;             // Definindo o intervalo de tempo entre as amostras para o cálculo da integral e da derivada
+      lastProcess = millis();
       Int += error*dT;
-    
-    if(cont > N)
+      Int_L += error*dT;
       x = {temperatura};
-
-    
-    if(cont > N)
       u = kE*x + kI*Int + ajuste;
-                                         
+      u_L = kE_L*x + kI_L*Int_L + ajuste_L;
+                                    
 
       // Windup
 
@@ -98,6 +99,14 @@ void loop (){
         }else
           Int = Int;
 
+      if(error < 0.2 && error > -0.2){
+        if((255 - u_L(0))>255)
+          Int_L = Int_L/1.2;
+        else if((255 - u_L(0))<0)
+          Int_L = Int_L/1.2;
+        }else
+          Int_L = Int_L;
+
       // Delimitando o limite de atuação de acordo com o PWM
 
       if (u(0) > 255)
@@ -106,29 +115,32 @@ void loop (){
         u(0) = 0;
       else 
         u = u;
-     }else
-        u(0) = 0;
+
+     if(u_L(0) > 255)
+      u_L(0) = 255;
+     else if (u_L(0) < 0)
+        u_L(0) = 0;
+      else 
+        u_L = u_L;
+
+    }
+  
 
     // Alterando os setpoints ao decorrer da simulação
-
-  if (millis() > 400000 && millis() < (400000*2)){
-    setPoint = 31;
-    //MyPID.setSetPoint(setPoint);
-    //MyPID1.setSetPoint(setPoint);
   
-  }else if(millis() >= (400000*2) && millis() < (400000*3)){
-    setPoint = 29.5;
-    //MyPID.setSetPoint(setPoint);
-    //MyPID1.setSetPoint(setPoint);
-    
-  }else if(millis() >= (400000*4)){
-    setPoint = 30.5;
-    //MyPID1.setSetPoint(setPoint);
-  }
+    if (millis() > 400000 && millis() < (400000*2)){
+      setPoint = 31;  
+    }else if(millis() >= (400000*2) && millis() < (400000*3)){
+      setPoint = 29.5;    
+    }else if(millis() >= (400000*4)){
+      setPoint = 30.5;
+    }
 
-    
-
-   // Aplicando perturbações no sistema SISO
+    // Escrevendo cálculos para controlar os atuadores
+    int output = u(0);
+    //int output1 = u_L(0);
+    int output1 = 255;
+    // Aplicando perturbações no sistema SISO
 
     if((millis() >= (400000*0.6)) && (millis() <= (400000*0.61)))
       output1 = 128;
@@ -137,10 +149,11 @@ void loop (){
     else if (millis() >= (400000*4.6) && (millis() <= (400000*4.63)))
       output1 = 100;
     else
-      output1 = 255;
+      output1 = output1;
+      //output1 = u_L(0);
 
-    // Escrevendo cálculos para controlar os atuadores
-    int output = u(0);
+    
+    
     analogWrite(6,output);
     analogWrite(5,output1);
 
@@ -155,7 +168,8 @@ void loop (){
     Serial.print("\t");
     Serial.println(results[3]);
 
-    cont++;
+    lastTime = now;
+  }
 }
 
 double getError(double setPoint, double temperatura){
